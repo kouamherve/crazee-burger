5cc19417927e1c9548e837a2ab0bfebafc920d33
kouamherve <kouamherve01@gmail.com>
2023-12-04 15:01:18 -0500

    ✨ F01: App > Create form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Bonjour</div>;
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form>
+        <input type="text" placeholder="Entrez votre prénom" />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
