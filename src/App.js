520c4f5e6251ede6deff5646dede37fe34ad7056
kouamherve <kouamherve01@gmail.com>
2023-12-04 15:15:31 -0500

    :Fi♻️ F01: LOGIN > LoginPage > LoginForm

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import LoginPage from "./components/page/login/LoginPage";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Bonjour ${inputValue}`);
-    setInputValue("");
-  };
-
-  return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Entrez votre prénom..."
-          required
-          value={inputValue}
-          onChange={handleChange}
-        />
-        <button type="submit">Accédez à votre espace</button>
-      </form>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
---
+++ src/components/page/login/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  const [inputValue, setInputValue] = useStateF01("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit">Accédez à votre espace</button>
+      </form>
+    </div>
+  );
+}
