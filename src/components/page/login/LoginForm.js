d667618ea28d12f1d4b27e7c6605b6380f4b30bf
kouamherve <kouamherve01@gmail.com>
2023-12-05 10:29:56 -0500

    💄 F03: LOGIN > LoginForm > input > Add styling

--- src/components/page/login/LoginForm.js
+++ src/components/page/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaRegUserCircle } from "react-icons/fa";
 
 export default function LoginForm() {
   const [inputValue, setInputValue] = useState("");
@@ -16,22 +17,26 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="text-white text-center pt-20">
+    <div className="text-white text-center pt-20 flex flex-col items-center justify-center">
       <h1 className=" text-[48px] leading-[61px] font-amatic font-bold">
         Bienvenue chez nous !
       </h1>
-      <hr className=" border-[#F56A2C] border bg-[#F56A2C] w-[400px] h-1 mt-5 " />
+      <hr className=" border-[#F56A2C] border bg-[#F56A2C] w-full h-1 mt-5 " />
       <h2 className=" text-4xl font-amatic font-bold mt-10 mb-[18px] leading-[46px]">
         Connectez-vous
       </h2>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Entrez votre prénom..."
-          required
-          value={inputValue}
-          onChange={handleChange}
-        />
+        <div className="relative text-gray-400 focus-within:text-gray-600 flex items-center gap-1">
+          <FaRegUserCircle className=" absolute w-[15px] h-[15px] ml-5 pointer-events-none" />
+          <input
+            type="text"
+            placeholder="Entrez votre prénom..."
+            required
+            value={inputValue}
+            onChange={handleChange}
+            className=" py-[18px] pl-12 bg-white rounded-[5px] w-[400px] placeholder:text-gray-300 border-none focus:ring-2 focus:text-gray-500"
+          />
+        </div>
         <button type="submit">Accédez à votre espace</button>
       </form>
     </div>
