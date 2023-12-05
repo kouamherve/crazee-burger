b0f92c6042d7632426bd151d5cf86a9ba867b7a9
kouamherve <kouamherve01@gmail.com>
2023-12-05 13:00:17 -0500

    💄 F03: LOGIN > LoginForm > Create reusable <Input />

--- src/components/page/login/LoginForm.js
+++ src/components/page/login/LoginForm.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FaRegUserCircle } from "react-icons/fa";
 import { MdNavigateNext } from "react-icons/md";
+import Input from "../../reusable-ui/Input";
+import { FaRegUserCircle } from "react-icons/fa";
 
 export default function LoginForm() {
   const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
+
+  const inputClassName =
+    "py-[18px] pl-12 pr-6 bg-white rounded-[5px] w-[400px] h-[55px] placeholder:text-gray-300 border-none focus:ring-2 focus:text-gray-500";
+  const iconClassName = "absolute w-[15px] h-[15px] ml-5 pointer-events-none";
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
@@ -19,30 +24,27 @@ export default function LoginForm() {
 
   return (
     <div className="text-white text-center mt-14 flex flex-col items-center justify-center max-w-[400px]">
-      <h1 className=" text-[48px] leading-[61px] font-amatic font-bold">
+      <h1 className="text-[48px] leading-[61px] font-amatic font-bold">
         Bienvenue chez nous !
       </h1>
-      <hr className=" border-[#F56A2C] border bg-[#F56A2C] w-full h-1 mt-8 " />
-      <h2 className=" text-4xl font-amatic font-bold mt-10 mb-[18px] leading-[46px]">
+      <hr className="border-[#F56A2C] border bg-[#F56A2C] w-full h-1 mt-8 " />
+      <h2 className="text-4xl font-amatic font-bold mt-10 mb-[18px] leading-[46px]">
         Connectez-vous
       </h2>
       <form onSubmit={handleSubmit} className=" max-w-md">
-        <div className="relative text-gray-400 focus-within:text-gray-600 flex items-center gap-1">
-          <FaRegUserCircle className=" absolute w-[15px] h-[15px] ml-5 pointer-events-none" />
-          <input
-            type="text"
-            placeholder="Entrez votre prénom..."
-            required
-            value={inputValue}
-            onChange={handleChange}
-            className=" py-[18px] pl-12 pr-6 bg-white rounded-[5px] w-[400px] h-[55px] placeholder:text-gray-300 border-none focus:ring-2 focus:text-gray-500"
-          />
-        </div>
+        <Input
+          className={inputClassName}
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={"Entrez votre prénom..."}
+          required
+          Icon={<FaRegUserCircle className={iconClassName} />}
+        />
         <button
           type="submit"
-          className="flex items-center justify-center gap-1 text-[15px] leading-[15px] font-bold bg-[#FF9F1B] h-[53px] border border-[#FF9F1B] rounded-[5px] w-full mt-[18px]"
+          className="flex items-center justify-center gap-1 text-[15px] leading-[15px] font-bold bg-[#FF9F1B] h-[53px] border border-[#FF9F1B] rounded-[5px] w-[400px] mt-[18px]"
         >
-          <span className=" h-[17px]">Accédez à votre espace</span>
+          <span className="h-[17px]">Accédez à votre espace</span>
           <MdNavigateNext className="w-5 h-5" />
         </button>
       </form>
---
+++ src/components/reusable-ui/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Input({
+  value,
+  onChange,
+  Icon,
+  className,
+  ...extraProps
+}) {
+  return (
+    <div className="relative text-gray-400 focus-within:text-gray-600 flex items-center gap-1">
+      {Icon}
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        className={className}
+        {...extraProps}
+      />
+    </div>
+  );
+}
