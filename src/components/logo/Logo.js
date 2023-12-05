fefd3e1d3426849189f4038fe5a0031359213860
kouamherve <kouamherve01@gmail.com>
2023-12-05 09:18:15 -0500

    💄 F03: LOGIN > Add styling and background image and created <Logo />

--- src/components/logo/Logo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Logo() {
-  return <div className="border bg-blue-500">LogoPage</div>;
-}
--- src/components/page/login/LoginPage.js
+++ src/components/page/login/LoginPage.js
@@ -1,9 +1,9 @@
-import Logo from "../../logo/Logo";
+import Logo from "../../reusable-ui/Logo";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className=" border bg-red-500 h-screen flex flex-col items-center justify-center">
+    <div className=" bg-[url('/src/images/burger-with-fries-background.jpg')] bg-cover bg-no-repeat bg-blend-overlay bg-black/50 h-screen flex flex-col items-center justify-center">
       <Logo />
       <LoginForm />
     </div>
