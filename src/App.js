9ff1d07e7a552ffba6fd106f461591761156f3f7
kouamherve <kouamherve01@gmail.com>
2023-12-05 08:36:55 -0500

    ✨ F02: Redirection from/to LoginPage to/from OrderPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/page/login/LoginPage";
+import OrderPage from "./components/page/order/OrderPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="order" element={<OrderPage />} />
     </Routes>
   );
 }
---
+++ src/components/page/order/OrderPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function OrderPage() {
+  return (
+    <div>
+      <h1>OrderPage</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </div>
+  );
+}
