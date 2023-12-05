381cb26b24b7018c5c00137fb33a558c4a41ab43
kouamherve <kouamherve01@gmail.com>
2023-12-05 09:07:24 -0500

    ✨ F03: Layout defined and <Logo /> created

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Logo() {
+  return <div className="border bg-blue-500">LogoPage</div>;
+}
--- src/components/page/login/LoginPage.js
+++ src/components/page/login/LoginPage.js
@@ -1,5 +1,11 @@
+import Logo from "../../logo/Logo";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className=" border bg-red-500 h-screen flex flex-col items-center justify-center">
+      <Logo />
+      <LoginForm />
+    </div>
+  );
 }
