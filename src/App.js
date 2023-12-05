3e629b4c71d3d7c34ec9cc56f6e609c75516e2e6
kouamherve <kouamherve01@gmail.com>
2023-12-05 08:42:13 -0500

    ✨ F02: Create ErrorPage with button to return to LoginPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/page/login/LoginPage";
 import OrderPage from "./components/page/order/OrderPage";
+import ErrorPage from "./components/page/error/ErrorPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
