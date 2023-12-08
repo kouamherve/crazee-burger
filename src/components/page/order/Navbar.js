822b88419405e3f99ea8338c5b1d313859509991
kouamherve <kouamherve01@gmail.com>
2023-12-08 00:41:26 -0500

    ✨ F04: ORDER > Navbar > Add refreshPage

--- src/components/page/order/Navbar.js
+++ src/components/page/order/Navbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import Logo from "../../reusable-ui/Logo";
 import NavbarRightSide from "./NavbarRightSide";
 
-export default function Navbar({ username }) {
+export default function Navbar({ username, onClick }) {
   return (
     <nav className="px-5 bg-white w-full h-[95px] flex items-center justify-between rounded-t-[15px]">
-      <Logo />
+      <button onClick={onClick}>
+        <Logo />
+      </button>
       <NavbarRightSide username={username} />
     </nav>
   );
--- src/components/page/order/OrderPage.js
+++ src/components/page/order/OrderPage.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import { refreshPage } from "../../../utils";
 
 export default function OrderPage() {
   const { username } = useParams();
   return (
     <div className=" bg-primary font-openSans h-screen flex flex-col items-center p-6">
       <div className="w-full ">
-        <Navbar username={username} />
+        <Navbar username={username} onClick={refreshPage} />
         <Main />
       </div>
     </div>
