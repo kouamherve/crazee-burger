edb5090be2c1ea3363184db48bad7ce96be1ff41
kouamherve <kouamherve01@gmail.com>
2023-12-07 22:39:13 -0500

    ✨ F04: ORDER > OrderPage > Create layout

--- src/components/page/order/OrderPage.js
+++ src/components/page/order/OrderPage.js
@@ -4,11 +4,21 @@ import { Link, useParams } from "react-router-dom";
 export default function OrderPage() {
   const { username } = useParams();
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+    <div className=" bg-primary h-screen p-5 flex flex-col items-center">
+      <div className="w-full h-[90%]">
+        <nav className=" bg-green w-full">
+          Navbar
+          <div>LeftSide</div>
+          <div>
+            RightSide
+            <h1>Bonjour {username}</h1>
+            <Link to="/">
+              <button>Déconnexion</button>
+            </Link>
+          </div>
+        </nav>
+        <div className=" bg-blue h-full">Main</div>
+      </div>
     </div>
   );
 }
