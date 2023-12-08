fd371bc9e85b7bd18e38468334e28be5ca8c66b6
kouamherve <kouamherve01@gmail.com>
2023-12-07 20:40:15 -0500

    🐛 F04: LOGIN > LoginPage > Fix bug logo

--- src/components/reusable-ui/Logo.js
+++ src/components/reusable-ui/Logo.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 export default function Logo({ size }) {
-  const logo = `${size} uppercase flex items-center justify-center gap-1 text-[36px] text-primary tracking-[1.5px] font-amatic leading-[46px] font-bold;
-  `;
+  const classes = {
+    logo: `${size}  flex items-center justify-center gap-1`,
+    logoTitle:
+      "uppercase text-[36px] text-primary tracking-[1.5px] font-amatic leading-[46px] font-bold",
+  };
 
   return (
-    <div className={logo}>
-      <h1>crazee</h1>
+    <div className={classes.logo}>
+      <h1 className={classes.logoTitle}>crazee</h1>
       <img src="./logo.png" alt="Logo" className="w-[80px] h-[60px]" />
-      <h1>burger</h1>
+      <h1 className={classes.logoTitle}>burger</h1>
     </div>
   );
 }
