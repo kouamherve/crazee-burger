a008276d678c89f03b6bab6465b16cef70e09c4f
kouamherve <kouamherve01@gmail.com>
2023-12-07 20:32:04 -0500

    ♻️ F04: LOGIN > LoginPage > Add updated <Logo />

--- src/components/page/login/LoginPage.js
+++ src/components/page/login/LoginPage.js
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className=" bg-[url('/src/images/burger-with-fries-background.jpg')] bg-center bg-cover bg-no-repeat bg-blend-overlay bg-black/50 h-screen flex flex-col items-center justify-center">
+    <div className=" space-y-24 bg-[url('/src/images/burger-with-fries-background.jpg')] bg-center bg-cover bg-no-repeat bg-blend-overlay bg-black/50 h-screen flex flex-col items-center justify-center">
       <Logo size={"scale-250"} />
       <LoginForm />
     </div>
