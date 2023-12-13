9172d3f0758b7bb808eee95950edc3dbbb694b8d
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-13 18:55:21 -0500

    🚧 F05: ORDER > Menu > Product > Add layout

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {  fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -7,7 +7,10 @@ export default function Menu() {
   return (
     <div className="w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px] justify-items-center overflow-y-auto">
       {menu.map((product) => (
-        <div className=" w-[240px] h-[330px] border border-lime-950 bg-red">{product.title}</div>
+        <div className=" w-[240px] h-[330px] border border-gray-300 flex flex-col items-center">
+          <div className="border-2 border-yellow-950 w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">Image</div>
+          <div className="border-2 border-sky-950 w-[200px] h-[110px]">Info-text</div>
+        </div>
       ))}
     </div>
   );
