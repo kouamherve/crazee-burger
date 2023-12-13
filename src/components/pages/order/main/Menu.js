6deb8954e77ce8e301dc22653413aef24c691977
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-13 18:38:05 -0500

    🚧 F05: ORDER > Menu > Add styling

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -5,7 +5,7 @@ export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
 
   return (
-    <div className=" bg-slate-500 w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px]  justify-items-center overflow-auto">
+    <div className="w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px] justify-items-center overflow-y-auto">
       {menu.map((product) => (
         <div className=" w-[240px] h-[330px] border border-lime-950 bg-red">{product.title}</div>
       ))}
