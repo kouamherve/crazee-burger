239e16989f6a539f0282422840f7d6cba09ba70d
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-13 18:29:30 -0500

    🚧 F05: ORDER > Menu > Add grid Products

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { fakeMenu1, fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import {  fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
 
   return (
-    <div className=" bg-slate-500 w-full">
+    <div className=" bg-slate-500 w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px]  justify-items-center overflow-auto">
       {menu.map((product) => (
-        <div>{product.title}</div>
+        <div className=" w-[240px] h-[330px] border border-lime-950 bg-red">{product.title}</div>
       ))}
     </div>
   );
