e29c5ade4c55583d97fdc693e528e6617bd947b1
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-14 00:54:19 -0500

    ♻️ F05: ORDER > Menu > Refactoring style

--- src/components/pages/order/main/Card.js
+++ src/components/pages/order/main/Card.js
@@ -1,31 +1,46 @@
 import React from "react";
 import PrimaryButton from "../../../reusable-ui/PrimaryButton";
 import { formatPrice } from "../../../../utils/maths";
+import clsx from "clsx";
 
 export default function Card({ imageSource, title, price }) {
+  const classes = {
+    cardClassName: clsx(
+      "w-[240px] h-[330px]",
+      "flex flex-col items-center",
+      "bg-white rounded-[15px] shadow-card"
+    ),
+    imageContainer: "w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]",
+    image: "object-contain object-center w-full h-full",
+    infoTextContainer: "w-[200px] h-[110px] flex flex-col space-y-5",
+    infoTextTitle: clsx(
+      "w-full h-[46px]",
+      "text-[36px] truncate text-dark leading-[45.4px]",
+      "font-amatic font-bold"
+    ),
+    descriptionContainer: clsx(
+      "flex items-center justify-between",
+      "px-[5px] pb-[5px]"
+    ),
+    priceClassName: "text-[16px] text-primary leading-[22px] font-normal",
+    btnClassName: clsx(
+      "text-[11px] leading-[12px] text-white font-bold",
+      "border border-primary bg-primary hover:bg-primary/75 rounded-[5px]",
+      "w-[95px] h-[38px]",
+      "active:bg-white active:text-primary"
+    ),
+  };
+
   return (
-    <div className=" shadow-card bg-white w-[240px] h-[330px] flex flex-col items-center rounded-[15px]">
-      <div className=" w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">
-        <img
-          src={imageSource}
-          alt={title}
-          className="object-contain object-center w-full h-full"
-        />
+    <div className={classes.cardClassName}>
+      <div className={classes.imageContainer}>
+        <img src={imageSource} alt={title} className={classes.image} />
       </div>
-      <div className="w-[200px] h-[110px] flex flex-col space-y-5">
-        <div className="w-full h-[46px] text-[36px] truncate text-dark leading-[45.4px] font-amatic font-bold">
-          {title}
-        </div>
-        <div className=" flex items-center justify-between px-[5px] pb-[5px]">
-          <span className=" text-[16px] text-primary leading-[22px] font-normal ">
-            {formatPrice(price)}
-          </span>
-          <PrimaryButton
-            label={"Ajouter"}
-            className={
-              "text-[11px] leading-[12px] text-white w-[95px] h-[38px] font-bold bg-primary hover:bg-primary/75 border border-primary rounded-[5px] active:bg-white active:text-primary"
-            }
-          />
+      <div className={classes.infoTextContainer}>
+        <div className={classes.infoTextTitle}>{title}</div>
+        <div className={classes.descriptionContainer}>
+          <span className={classes.priceClassName}>{formatPrice(price)}</span>
+          <PrimaryButton label={"Ajouter"} className={classes.btnClassName} />
         </div>
       </div>
     </div>
--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import Card from "./Card";
+import clsx from "clsx";
 
 export default function Menu() {
+  const menuClassName = clsx(
+    "w-full h-full shadow-xlInner bg-background_white  p-[50px] rounded-b-[15px]",
+    "grid grid-cols-4 gap-x-[85px] gap-y-[60px] justify-items-center",
+    "overflow-auto"
+  );
   const [menu, setMenu] = useState(fakeMenu2);
 
   return (
-    <div className="overflow-auto w-full h-full shadow-xlInner bg-background_white grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px] justify-items-center rounded-b-[15px]">
+    <div className={menuClassName}>
       {menu.map((product) => (
         <Card
           imageSource={product.imageSource}
