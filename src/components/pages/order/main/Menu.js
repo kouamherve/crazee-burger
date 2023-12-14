834dcdaa1c1bfc721f433dc7632fdcfdf6706b08
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-13 19:18:46 -0500

    🚧 F05: ORDER > Menu > Product > Info-text > Add layout

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import Product from "./Product";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -7,12 +8,7 @@ export default function Menu() {
   return (
     <div className="w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px] justify-items-center overflow-y-auto">
       {menu.map((product) => (
-        <div className=" w-[240px] h-[330px] border border-gray-300 flex flex-col items-center">
-          <div className="border-2 border-yellow-950 w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">
-            <img src={product.imageSource} alt={product.title} className=" object-contain object-center w-full h-full"/>
-          </div>
-          <div className="border-2 border-sky-950 w-[200px] h-[110px]">Info-text</div>
-        </div>
+        <Product imageSource={product.imageSource} title={product.title}/>
       ))}
     </div>
   );
---
+++ src/components/pages/order/main/Product.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Product({imageSource, title}) {
+  return (
+    <div className=" w-[240px] h-[330px] border border-gray-300 flex flex-col items-center">
+          <div className="border-2 border-yellow-950 w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">
+            <img src={imageSource} alt={title} className=" object-contain object-center w-full h-full"/>
+          </div>
+          <div className="w-[200px] h-[110px] flex flex-col space-y-5">
+            <div className='w-full h-[46px] border border-gray-900'>Title</div>
+            <div className='border border-gray-900'>Description</div>
+          </div>
+        </div>
+  )
+}
