7090a768e23882dd316f96c17372835f670b24a7
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-13 19:05:15 -0500

    🚧 F05: ORDER > Menu > Product > Add image styling

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -8,7 +8,9 @@ export default function Menu() {
     <div className="w-full grid grid-cols-4 gap-x-[85px] gap-y-[60px] p-[50px] justify-items-center overflow-y-auto">
       {menu.map((product) => (
         <div className=" w-[240px] h-[330px] border border-gray-300 flex flex-col items-center">
-          <div className="border-2 border-yellow-950 w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">Image</div>
+          <div className="border-2 border-yellow-950 w-[200px] h-[145px] mx-5 mt-[50px] mb-[15px]">
+            <img src={product.imageSource} alt={product.title} className=" object-contain object-center w-full h-full"/>
+          </div>
           <div className="border-2 border-sky-950 w-[200px] h-[110px]">Info-text</div>
         </div>
       ))}
