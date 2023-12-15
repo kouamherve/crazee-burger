f55065ce19e49be9b84012c8a185478b32d62dd1
Herve Kenmegne Kouam <kouamherve01@gmail.com>
2023-12-15 11:16:38 -0500

    💄 F06: Profile > Fix button position "Se deconnecter"

--- src/components/pages/order/navbar/Profile.js
+++ src/components/pages/order/navbar/Profile.js
@@ -9,11 +9,11 @@ export default function Profile({ username }) {
   );
 
   return (
-    <div className=" ">
-      <h1 className=" text-[16px] leading-[22px]">
+    <div className=" flex flex-col ">
+      <h1 className="text-[16px] leading-[22px]">
         Hey, <b className=" text-primary">{username}</b>
       </h1>
-      <Link to="/">
+      <Link to="/" className=" text-end">
         <button className={btnClassName}>Se déconnecter</button>
       </Link>
     </div>
