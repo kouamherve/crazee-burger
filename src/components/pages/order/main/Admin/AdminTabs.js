8d04fddb4e67d97f41c7dfeba415833d8a5ba662
Herve Kouam <kouamherve01@gmail.com>
2023-12-23 17:07:27 -0500

    ✨ F07 : ADMIN > Tab > Add label

--- src/components/pages/order/main/Admin/AdminTabs.js
+++ src/components/pages/order/main/Admin/AdminTabs.js
@@ -1,5 +1,7 @@
 import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 export default function AdminTabs({ isCollapse, setIsCollapse }) {
   const handleClicK = () => {
@@ -7,17 +9,38 @@ export default function AdminTabs({ isCollapse, setIsCollapse }) {
   };
 
   return (
-    <div>
+    <div className="flex gap-px">
       <Tab
+        label=""
+        Icon={isCollapse ? <FiChevronUp /> : <FiChevronDown />}
         onClick={handleClicK}
         btn_tab={
           isCollapse
             ? " text-white bg-background_dark border border-background_dark"
             : "bg-white text-greySemiDark border border-greyLight"
         }
-      >
-        {isCollapse ? <FiChevronUp /> : <FiChevronDown />}
-      </Tab>
+      />
+
+      <Tab
+        label="Ajouter un produit"
+        Icon={<AiOutlinePlus />}
+        onClick={handleClicK}
+        btn_tab={
+          isCollapse
+            ? "text-white bg-background_dark border border-background_dark"
+            : "bg-white text-greySemiDark border border-greyLight"
+        }
+      />
+      <Tab
+        label="Modifier un produit"
+        Icon={<MdModeEditOutline />}
+        onClick={handleClicK}
+        btn_tab={
+          isCollapse
+            ? "text-white bg-background_dark border border-background_dark"
+            : "bg-white text-greySemiDark border border-greyLight"
+        }
+      />
     </div>
   );
 }
--- src/components/reusable-ui/Tab.js
+++ src/components/reusable-ui/Tab.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function Tab({ onClick, children, btn_tab }) {
+export default function Tab({ label, onClick, Icon, btn_tab }) {
   return (
     <button
       onClick={onClick}
       className={`${btn_tab} h-11 rounded-t-md border-b-2 hover:border-b-transparent shadow-subtle  text-base px-[22px] relative left-[5%] flex items-center justify-center`}
     >
-      {children}
+      <div className="flex items-center justify-center gap-4">
+        <span>{Icon}</span>
+        {label && <span>{label}</span>}
+      </div>
     </button>
   );
 }
