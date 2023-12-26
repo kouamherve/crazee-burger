84cd71fdb7673ae203135026d06fd73384f5c9a1
Herve Kouam <kouamherve01@gmail.com>
2023-12-26 05:51:11 -0500

    ✨ F08: ADMINPANEL > Add layout

--- src/components/pages/order/main/Admin/AdminPanel.js
+++ src/components/pages/order/main/Admin/AdminPanel.js
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
 import OrderContext from "../../../../../context/OrderContext";
+import AddAdminPanel from "./AddAdminPanel";
+import EditAdminPanel from "./EditAdminPanel";
 
 export default function AdminPanel() {
   const { currentTabSelected } = useContext(OrderContext);
 
   return (
     <div className="h-64 pl-5 pt-4 bg-white font-openSans text-base font-normal border border-greyLight shadow-subtle">
-      {currentTabSelected === "" ? "" : <div>{currentTabSelected}</div>}
+      {currentTabSelected === "" ? (
+        ""
+      ) : currentTabSelected === "Ajouter un produit" ? (
+        <AddAdminPanel />
+      ) : currentTabSelected === "Modifier un produit" ? (
+        <EditAdminPanel />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
