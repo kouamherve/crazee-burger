17aaff5fca0f6c7581472b8dba2602a7c6f212d2
Herve Kouam <kouamherve01@gmail.com>
2024-01-18 15:05:01 -0500

    ✨ F09: Add Favicon and change the App name

--- src/components/pages/order/main/Admin/adminPanel/EditPanel.js
+++ src/components/pages/order/main/Admin/adminPanel/EditPanel.js
@@ -48,6 +48,10 @@ export default function EditPanel() {
                     />
                   );
                 })}
+                <span className=" font-openSans font-normal text-sm text-primary">
+                  Cliquer sur un produit du menu pour le modifier{" "}
+                  <span className="underline">en temps réel</span>
+                </span>
               </form>
             </div>
           </div>
