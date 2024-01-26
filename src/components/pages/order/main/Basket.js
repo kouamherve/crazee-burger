9850039aaf0a9825bff8a2739939213f63354388
Herve Kouam <kouamherve01@gmail.com>
2024-01-26 00:30:20 -0500

    ♻️ F10: MAIN > Refactoring style

--- src/components/pages/order/main/Basket.js
+++ src/components/pages/order/main/Basket.js
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Basket() {
   return (
-    <div className=" text-background_white flex flex-col h-[100%]">
-      <div className="h-[8%] bg-black font-amatic font-normal text-4xl tracking-widest px-4 text-primary flex justify-between items-center">
-        <span>total</span>
-        <span>0,00 €</span>
+    <div className=" text-background_white flex flex-col h-[100%] font-amatic">
+      <div className="h-[8%] bg-black text-4xl px-4 flex justify-between items-center">
+        <span className="text-primary/60">total</span>
+        <b>
+          <span className="text-primary">0,00 €</span>
+        </b>
       </div>
-      <div className="h-[84%] font-amatic font-normal text-4xl leading-[72px] text-greyBlue flex items-center justify-center">
+      <div className="h-[84%] text-4xl leading-[72px] text-greyBlue flex items-center justify-center">
         <span>Votre commande est vide.</span>
       </div>
-      <div className="h-[8%] bg-black font-amatic font-bold text-xl flex items-center justify-center">
+      <div className="h-[8%] bg-black font-bold text-xl text-white/60 flex items-center justify-center">
         <span>Codé avec ❤️ et React.JS</span>
       </div>
     </div>
