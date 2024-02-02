f96aba422f93ea34881736e1d8c75e5eb49763d0
Herve Kouam <kouamherve01@gmail.com>
2024-02-02 03:13:46 -0500

    🚧 F11: BASKET > Add scroll option on BasketBody

--- src/components/pages/order/main/Basket/BasketBody.js
+++ src/components/pages/order/main/Basket/BasketBody.js
@@ -4,7 +4,7 @@ import BasketCard from "../../../../reusable-ui/BasketCard";
 export default function BasketBody({ basketMenu }) {
   return (
     <div
-      className={`h-[84%]  bg-background_white text-greyBlue shadow-basket ${
+      className={`h-[84%]  bg-background_white text-greyBlue shadow-basket overflow-y-auto ${
         basketMenu.length === 0 &&
         "flex items-center justify-center text-4xl leading-[72px]"
       }`}
