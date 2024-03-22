2b21422e5c31fea02f6ad9c7326e79425980c969
Herve Kouam <kouamherve01@gmail.com>
2024-03-21 23:07:20 -0400

    ✨  F14: ADMIN > Add animation when isModeAdmin was activated

--- src/components/pages/order/main/Admin/Admin.js
+++ src/components/pages/order/main/Admin/Admin.js
@@ -2,14 +2,22 @@ import React, { useContext } from "react";
 import OrderContext from "../../../../../context/OrderContext";
 import AdminPanel from "./AdminPanel/AdminPanel";
 import AdminTabs from "./AdminPanel/AdminTabs";
+import { motion } from "framer-motion";
 
 export default function Admin() {
   const { isCollapse, setIsCollapse } = useContext(OrderContext);
 
   return (
-    <div className="absolute bottom-0 left-0 right-0 rounded-b-2xl">
+    <motion.div
+      key={isCollapse}
+      initial={{ y: "100%", opacity: 0.5 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      exit={{ opacity: 0.5, y: "100%", transition: { duration: 0.2 } }}
+      className="absolute bottom-0 left-0 right-0 rounded-b-2xl"
+    >
       <AdminTabs isCollapse={isCollapse} setIsCollapse={setIsCollapse} />
       {!isCollapse && <AdminPanel />}
-    </div>
+    </motion.div>
   );
 }
